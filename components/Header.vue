201fcb23215a10974efac865af6c0558fa7fe0c8
Jody Hoon-Starr <jody@protonmail.com>
2023-06-14 17:04:50 -0400

    style: icon order

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,15 @@
 <template>
   <div inline-flex cursor-default text-2xl font-300>
     <div flex flex-col children:m-auto>
-      <img inline-block w-48 src="/census.png">
+      <img inline-block w-48 src="/grants.png">
     </div>
     <div
       text="3xl gray4"
       m="x-4 y-auto"
-      i-carbon-add transform transition-all-500 hover:rotate-135
+      i-carbon-add
     />
     <div flex flex-col children:m-auto>
-      <img inline-block w-48 src="/grants.png">
+      <img inline-block w-48 src="/census.png">
     </div>
   </div>
 </template>
