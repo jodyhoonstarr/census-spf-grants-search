fed2be596486ed5c2faef457ca9679f71998f7e5
Jody Hoon-Starr <jody@protonmail.com>
2023-07-03 11:46:54 -0400

    style: font stack

--- uno.config.ts
+++ uno.config.ts
@@ -4,7 +4,6 @@ import {
   presetIcons,
   presetTypography,
   presetUno,
-  presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
@@ -14,6 +13,11 @@ export default defineConfig({
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
   ],
+  theme: {
+    colors: {
+      census: '#112e51',
+    },
+  },
   presets: [
     presetUno(),
     presetAttributify(),
@@ -21,13 +25,6 @@ export default defineConfig({
       scale: 1.2,
     }),
     presetTypography(),
-    presetWebFonts({
-      fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
-      },
-    }),
   ],
   transformers: [
     transformerDirectives(),
