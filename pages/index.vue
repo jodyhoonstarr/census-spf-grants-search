cb9151a1b4b839595836769148e5263a7a192cf6
Jody Hoon-Starr <jody@protonmail.com>
2023-07-05 17:24:31 -0400

    fix: bad link

--- pages/index.vue
+++ pages/index.vue
@@ -32,8 +32,8 @@
       <span relative w-full inline-flex md="w-auto">
 
         <NuxtLink
-          to="/lookup"
-          class="bg-secondary focus:ring-secondary w-full inline-flex items-center justify-center border border-transparent rounded-full px-8 py-4 text-base font-medium leading-6 text-white md:w-auto xl:px-10 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2"
+          to="/grant"
+          class="w-full inline-flex items-center justify-center border border-transparent rounded-full bg-secondary px-8 py-4 text-base font-medium leading-6 text-white md:w-auto xl:px-10 hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary"
         >
           Get Started
         </NuxtLink>
@@ -43,7 +43,7 @@
 
         <NuxtLink
           to="/about"
-          class="text-secondary w-full inline-flex items-center justify-center border border-transparent rounded-full bg-gray-100 px-8 py-4 text-base font-medium leading-6 md:w-auto hover:bg-gray-200 xl:px-10 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200"
+          class="w-full inline-flex items-center justify-center border border-transparent rounded-full bg-gray-100 px-8 py-4 text-base font-medium leading-6 text-secondary md:w-auto hover:bg-gray-200 xl:px-10 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200"
         >
           Learn More
         </NuxtLink>
